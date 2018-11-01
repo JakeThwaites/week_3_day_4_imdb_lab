eb9cce2e49af7ed0c831899df913077813b6a674
Jake Thwaites <jakethwaites@hotmail.co.uk>
2018-11-01 16:00:48 +0000

    finished extension

--- console.rb
+++ console.rb
@@ -4,9 +4,9 @@ require_relative('models/casting')
 
 require('pry-byebug')
 
-movie1 = Movie.new( {'title' => 'star_wars', 'genre' => 'action'} )
+movie1 = Movie.new( {'title' => 'star_wars', 'genre' => 'action', 'budget' => 20} )
 movie1.save()
-movie2 = Movie.new( {'title' => 'deadpool', 'genre' => 'action'} )
+movie2 = Movie.new( {'title' => 'deadpool', 'genre' => 'action', 'budget' => 20} )
 movie2.save()
 
 star1 = Star.new( {'first_name' => 'Mark', 'last_name' => 'Hammell'} )
@@ -15,7 +15,7 @@ star2 = Star.new( {'first_name' => 'Ryan', 'last_name' => 'Reynolds'} )
 star2.save()
 
 casting1 = Casting.new({'movie_id' => movie1.id, 'star_id' => star1.id, 'fee' => 5} )
-casting2 = Casting.new({'movie_id' => movie2.id, 'star_id' => star2.id, 'fee' => 10} )
+casting2 = Casting.new({'movie_id' => movie1.id, 'star_id' => star2.id, 'fee' => 10} )
 
 casting1.save
 casting2.save
--- models/movie.rb
+++ models/movie.rb
@@ -2,18 +2,19 @@ require_relative('../db/sql_runner')
 
 class Movie
 
-  attr_accessor :title, :genre
+  attr_accessor :title, :genre, :budget
   attr_reader :id
 
   def initialize(options)
     @title = options['title']
     @genre = options['genre']
     @id = options['id'].to_i if options['id']
+    @budget = options['budget']
   end
 
   def save()
-    sql = "INSERT INTO MOVIES (title, genre) VALUES ($1, $2) RETURNING id"
-    values = [@title, @genre]
+    sql = "INSERT INTO MOVIES (title, genre, budget) VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @genre, @budget]
     movie = SqlRunner.run(sql, values).first
     @id = movie['id'].to_i
   end
@@ -27,14 +28,27 @@ class Movie
 
   def stars()
     sql = "SELECT stars.*
-FROM stars
-INNER JOIN castings
-ON stars.id = castings.star_id
-WHERE movie_id = $1"
-  values = [@id]
-  stars = SqlRunner.run(sql, values)
-  return stars.map { |star| Star.new(stars) }
+  FROM stars
+  INNER JOIN castings
+  ON stars.id = castings.star_id
+  WHERE movie_id = $1"
+    values = [@id]
+    stars = SqlRunner.run(sql, values)
+    return stars.map { |star| Star.new(stars) }
+  end
 
+  def remaining_budget()
+
+    sql = "SELECT SUM(castings.fee)
+          FROM castings
+          INNER JOIN movies
+          ON movies.id = castings.movie_id
+          WHERE movie_id = $1"
+    values = [@id]
+    total_fee = SqlRunner.run(sql, values).first['sum']
+
+    # remaining = total_fee.map { |fee| Movie.new(fee) }
+    return @budget - total_fee
   end
 
 end
